44783578dac79dddc5f1a5db8c6a5bec5194df97
NOX-ZUHILL <suhilkhasib7@gmail.com>
2026-06-07 15:27:35 +0200

    Update NOX loader.lua

--- NOX loader.lua
+++ NOX loader.lua
@@ -1,1 +1,18 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/d062b00259041783b07237891aae4e29.lua"))()
+repeat task.wait() until game:IsLoaded()
+repeat task.wait() until game.Players.LocalPlayer
+_G.Loaded = true
+
+local placeid = game.PlaceId
+
+if placeid == 107646426076756 then
+	loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/d062b00259041783b07237891aae4e29.lua"))()
+elseif placeid == 96645548064314 then
+	loadstring(game:HttpGet("https://pastefy.app/mWeO5GdW/raw"))()
+else
+	game:GetService("StarterGui"):SetCore("SendNotification", {
+		Icon = "rbxassetid://108551171937093",
+		Title = "NOX",
+		Text = "Game Not Supported!",
+		Duration = 5,
+	})
+end
