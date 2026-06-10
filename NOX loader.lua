5f3f5259d00279cedc67834ea8594ab610f6ff38
NOX-ZUHILL <suhilkhasib7@gmail.com>
2026-06-10 02:01:13 +0200

    Update NOX loader.lua

--- NOX loader.lua
+++ NOX loader.lua
@@ -1,13 +1,13 @@
 repeat task.wait() until game:IsLoaded()
 repeat task.wait() until game.Players.LocalPlayer
 _G.Loaded = true
-
 local placeid = game.PlaceId
-
 if placeid == 107646426076756 then
 	loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/d062b00259041783b07237891aae4e29.lua"))()
 elseif placeid == 96645548064314 then
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/NOX-ZUHILL/NOX/refs/heads/main/Nox-Catch%20and%20Tame%20inf%20money.lua"))()
+	loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/d062b00259041783b07237891aae4e29.lua"))()
+elseif placeid == 79268393072444 then
+	loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/d062b00259041783b07237891aae4e29.lua"))()
 else
 	game:GetService("StarterGui"):SetCore("SendNotification", {
 		Icon = "rbxassetid://108551171937093",
