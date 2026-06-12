49188b8a441810deaa99e4eeb0aad1f97769b07f
NOX-ZUHILL <suhilkhasib7@gmail.com>
2026-06-12 22:43:02 +0200

    Update NOX loader.lua

--- NOX loader.lua
+++ NOX loader.lua
@@ -4,6 +4,8 @@ _G.Loaded = true
 local placeid = game.PlaceId
 if placeid==107646426076756 or placeid==96645548064314 or placeid==79268393072444 then
 	loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/d062b00259041783b07237891aae4e29.lua"))()
+elseif placeid==83569851223739 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/NOX-ZUHILL/-1-Speed-Evolve/refs/heads/main/%2B1%20Speed%20Evolve.obfuscated.lua"))()
 else
 	game:GetService("StarterGui"):SetCore("SendNotification",{
 		Icon="rbxassetid://108551171937093",
