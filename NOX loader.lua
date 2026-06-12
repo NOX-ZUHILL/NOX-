cb83b2f808c2566611d928b96dab36a6a3b59324
NOX-ZUHILL <suhilkhasib7@gmail.com>
2026-06-12 17:10:42 +0200

    Update NOX loader.lua

--- NOX loader.lua
+++ NOX loader.lua
@@ -2,17 +2,13 @@ repeat task.wait() until game:IsLoaded()
 repeat task.wait() until game.Players.LocalPlayer
 _G.Loaded = true
 local placeid = game.PlaceId
-if placeid == 107646426076756 then
-	loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/d062b00259041783b07237891aae4e29.lua"))()
-elseif placeid == 96645548064314 then
-	loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/d062b00259041783b07237891aae4e29.lua"))()
-elseif placeid == 79268393072444 then
+if placeid==107646426076756 or placeid==96645548064314 or placeid==79268393072444 then
 	loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/d062b00259041783b07237891aae4e29.lua"))()
 else
-	game:GetService("StarterGui"):SetCore("SendNotification", {
-		Icon = "rbxassetid://108551171937093",
-		Title = "NOX",
-		Text = "Game Not Supported!",
-		Duration = 5,
+	game:GetService("StarterGui"):SetCore("SendNotification",{
+		Icon="rbxassetid://108551171937093",
+		Title="NOX",
+		Text="Game Not Supported!",
+		Duration=5,
 	})
 end
